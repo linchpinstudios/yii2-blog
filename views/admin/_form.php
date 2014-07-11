9e0f9c916816fe1180969360f650ba2dc73ec267
Josh Hagel <joshhagel@linchpinstudios.com>
2014-07-11 15:52:10 -0700

    Added TinyMCE

--- views/admin/_form.php
+++ views/admin/_form.php
@@ -2,7 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use yii\jui\DatePicker;
+use dosamigos\tinymce\TinyMce;
+use dosamigos\fileupload\FileUploadUI;
 
 /**
  * @var yii\web\View $this
@@ -14,33 +15,41 @@ use yii\jui\DatePicker;
 <div class="blog-posts-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
+    
+    <?= $form->field($model, 'title')->textInput(['maxlength' => 555]) ?>
+    
+    
+    <?= $form->field($model, 'body')->widget(TinyMce::className(), [
+        'options' => ['rows' => 10],
+        'clientOptions' => [
+            'plugins' => [
+                "advlist autolink lists link charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table contextmenu paste"
+            ],
+            'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+        ]
+    ]);?>
+    
+    <?= FileUploadUI::widget([
+            'model' => $model,
+            'attribute' => 'thumbnail',
+            'url' => ['/blog/admin/upload'],
+            'gallery' => false,
+            'fieldOptions' => [
+                    'accept' => 'image/*'
+            ],
+            'clientOptions' => [
+                    'maxFileSize' => 2000000
+            ]
+        ]);
+    ?>
+    
     <?= $form->field($model, 'user_id')->textInput() ?>
 
     <?= $form->field($model, 'comments')->textInput() ?>
 
-    <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
-
     <?= $form->field($model, 'excerpt')->textarea(['rows' => 6]) ?>
-
-    <?= $form->field($model, 'date')->textInput() ?>
-    
-    <?php
-    echo DatePicker::widget([
-            'model' => $model,
-            'attribute' => 'date',
-            'clientOptions' => [
-                'dateFormat' => 'yy-mm-dd HH:MM:SS',
-            ],
-        ]);
-    ?>
-    <?= $form->field($model, 'title')->textInput(['maxlength' => 555]) ?>
-
-    <?= $form->field($model, 'thumbnail')->textInput(['maxlength' => 555]) ?>
-
-    <?= $form->field($model, 'status')->textInput(['maxlength' => 20]) ?>
-
-    <?= $form->field($model, 'slug')->textInput(['maxlength' => 45]) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
