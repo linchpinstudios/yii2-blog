74413b70d1d4f1822ff4bad3b74d1fd7f17111de
Josh Hagel <joshhagel@linchpinstudios.com>
2014-09-26 17:11:37 -0700

    Fixes

--- js/blog.js
+++ js/blog.js
@@ -13,4 +13,47 @@ $(function() {
     
     
     
+    $('#create_category').submit(function(e){
+        
+        e.preventDefault();
+        
+        createCategory($(this));
+        
+    });
+    
+    
+    
+    
+    
 });
+
+
+
+
+
+
+function createCategory(form) {
+
+    if($(form).find('.has-error').length) {
+        return false;
+    }
+    
+    $.ajax({
+        url: form.attr('action'),
+        type: 'post',
+        data: form.serialize(),
+        success: function(data) {
+            if(data.error){
+                $('.field-blogterms-name').addClass('has-error');
+                $('.field-blogterms-name .help-block').text(data.error.name);
+            }
+            if(data.success){
+                $('#create-category-form').toggle();
+                
+                $('#categories-con').append('<div class=\"checkbox\"><label><input type=\"checkbox\" name=\"categories[]\" checked=\"checked\" value=\"'+data.model.id+'\"> '+data.model.name+'</label></div>');
+            }
+        }
+    });
+    
+    return false;
+}
--- views/admin/_form.php
+++ views/admin/_form.php
@@ -96,34 +96,10 @@ $tfArray = [
                         
                         <?php
                             $form = ActiveForm::begin([
-                                'action' => ['blogterms/createcategory'],
+                                'action'                    => ['blogterms/createcategory'],
                                 'enableAjaxValidation'      => false,
                                 'enableClientValidation'    => true,
-                                'afterValidate'             => "
-                                    function(form) {
-                                        if($(form).find('.has-error').length) {
-                                            return false;
-                                        }
-                                        
-                                        $.ajax({
-                                            url: form.attr('action'),
-                                            type: 'post',
-                                            data: form.serialize(),
-                                            success: function(data) {
-                                                if(data.error){
-                                                    $('.field-blogterms-name').addClass('has-error');
-                                                    $('.field-blogterms-name .help-block').text(data.error.name);
-                                                }
-                                                if(data.success){
-                                                    $('#create-category-form').toggle();
-                                                    
-                                                    $('#categories-con').append('<div class=\"checkbox\"><label><input type=\"checkbox\" name=\"categories[]\" checked=\"checked\" value=\"'+data.model.id+'\"> '+data.model.name+'</label></div>');
-                                                }
-                                            }
-                                        });
-                                        
-                                        return false;
-                                    }",
+                                'id'                        => 'create_category',
                             ]);
                         ?>
                             <?= $form->field($terms, 'name')->textInput(['maxlength' => 255]) ?>
