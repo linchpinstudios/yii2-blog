4e7234bcb32014234f58b6b665cdadde62825909
Josh Hagel <joshhagel@linchpinstudios.com>
2015-03-30 22:17:26 -0700

    Blog

--- js/blog.js
+++ js/blog.js
@@ -7,7 +7,7 @@ function createCategory(form) {
     if($(form).find('.has-error').length) {
         return false;
     }
-    
+
     $.ajax({
         url: form.attr('action'),
         type: 'post',
@@ -16,15 +16,16 @@ function createCategory(form) {
             if(data.error){
                 $('.field-blogterms-name').addClass('has-error');
                 $('.field-blogterms-name .help-block').text(data.error.name);
+                $('#myModal').modal('hide');
             }
             if(data.success){
                 $('#create-category-form').toggle();
-                
+
                 $('#categories-con').append('<div class=\"checkbox\"><label><input type=\"checkbox\" name=\"categories[]\" checked=\"checked\" value=\"'+data.model.id+'\"> '+data.model.name+'</label></div>');
             }
         }
     });
-    
+
     return false;
 }
 
@@ -37,7 +38,7 @@ function insertComment(form) {
     if($(form).find('.has-error').length) {
         return false;
     }
-    
+
     $.ajax({
         url: form.attr('action'),
         type: 'post',
@@ -50,46 +51,48 @@ function insertComment(form) {
             }
         }
     });
-    
+
     return false;
 
 }
 
 
 $(function() {
-    
-    
+
+
     /* Toggle Create Form
-     * 
+     *
      * Show and hide create category form
-     */
+     *
     $('#add-category').click(function(e){
         e.preventDefault();
         $('#create-category-form').toggle();
-    });
-    
-    
-    
+    });*/
+
+
+
     $('#create_category').submit(function(e){
-        
+
         e.preventDefault();
-        
+        e.stopImmediatePropagation();
+
         createCategory($(this));
-        
+
     });
-    
-    
-    
+
+
+
     $('#add_comment').submit(function(e){
-        
+
         e.preventDefault();
-        
+        e.stopImmediatePropagation();
+
         insertComment($(this));
-        
+
     });
-    
-    
-    
-    
-    
+
+
+
+
+
 });
