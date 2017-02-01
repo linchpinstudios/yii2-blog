65e4884d7a70cdf53466791e79d09b5216c01852
Elvira Sheina <elle.uz@gmail.com>
2017-02-01 14:37:14 +0500

    Custom render function for RecentPosts Widget

--- widgets/RecentPosts.php
+++ widgets/RecentPosts.php
@@ -14,6 +14,9 @@ use yii\web\UrlManager;
 
 class RecentPosts extends \yii\base\Widget
 {
+	//@var \Closure an anonymous function with the signature `function ($posts)` which render post list
+	public $render;
+
 	public function run()
 	{
 		if(isset(\Yii::$app->params['recentBlogPostsWidget']) && is_int(\Yii::$app->params['recentBlogPostsWidget'])) {
@@ -23,14 +26,18 @@ class RecentPosts extends \yii\base\Widget
 		}
 
 		$posts = BlogPosts::find()
-				->orderBy('date_gmt desc')
-				->limit($limit)
-				->all();
+			->orderBy('date_gmt desc')
+			->limit($limit)
+			->all();
 
-		if (empty($posts)) {
-			echo '<p>No posts to display.</p>';
+		if ($this->render instanceof \Closure) {
+			call_user_func($this->render, $posts);
 		} else {
-			echo '<ul class="list-unstyled">' . $this->renderPosts($posts) . '</ul>';
+			if (empty($posts)) {
+				echo '<p>No posts to display.</p>';
+			} else {
+				echo '<ul class="list-unstyled">' . $this->renderPosts($posts) . '</ul>';
+			}
 		}
 	}
 
@@ -46,13 +53,13 @@ class RecentPosts extends \yii\base\Widget
 
 	public function renderPost($post)
 	{
-	    $postRender = '<div class="row"><article>';
-            //$postRender .= Html::a('',['blogposts/view', 'id' => $post->id, 'urlSlug' => $post->urlSlug, 'year' => date('Y',strtotime($post->date_gmt)), 'month' => date('m',strtotime($post->date_gmt)), 'day' => date('d',strtotime($post->date_gmt))],['class'=>'recentPostThumnail','style'=>'background-image:url('.$post->thumbnail.')']);
-	    $postRender .= '<h4>' . Html::a(Html::encode($post->title), ['blogposts/view', 'id' => $post->id, 'year' => date('Y',strtotime($post->date_gmt)), 'month' => date('m',strtotime($post->date_gmt)), 'day' => date('d',strtotime($post->date_gmt))]) . '</h4>'; //'urlSlug' => $post->urlSlug
-	    $postRender .= '<p>'.$post->excerpt.'</p>';
-            $postRender .= '<p class="text-right">'.Html::a('Read Article',['blogposts/view', 'id' => $post->id, 'year' => date('Y',strtotime($post->date_gmt)), 'month' => date('m',strtotime($post->date_gmt)), 'day' => date('d',strtotime($post->date_gmt))],['class'=>'btn btn-success btn-xs']).'</p>'; //, 'urlSlug' => $post->urlSlug
-	    $postRender .= '</article></div>';
-	
+		$postRender = '<div class="row"><article>';
+		//$postRender .= Html::a('',['blogposts/view', 'id' => $post->id, 'urlSlug' => $post->urlSlug, 'year' => date('Y',strtotime($post->date_gmt)), 'month' => date('m',strtotime($post->date_gmt)), 'day' => date('d',strtotime($post->date_gmt))],['class'=>'recentPostThumnail','style'=>'background-image:url('.$post->thumbnail.')']);
+		$postRender .= '<h4>' . Html::a(Html::encode($post->title), ['blogposts/view', 'id' => $post->id, 'year' => date('Y',strtotime($post->date_gmt)), 'month' => date('m',strtotime($post->date_gmt)), 'day' => date('d',strtotime($post->date_gmt))]) . '</h4>'; //'urlSlug' => $post->urlSlug
+		$postRender .= '<p>'.$post->excerpt.'</p>';
+		$postRender .= '<p class="text-right">'.Html::a('Read Article',['blogposts/view', 'id' => $post->id, 'year' => date('Y',strtotime($post->date_gmt)), 'month' => date('m',strtotime($post->date_gmt)), 'day' => date('d',strtotime($post->date_gmt))],['class'=>'btn btn-success btn-xs']).'</p>'; //, 'urlSlug' => $post->urlSlug
+		$postRender .= '</article></div>';
+
 		return $postRender;
 	}
 }
